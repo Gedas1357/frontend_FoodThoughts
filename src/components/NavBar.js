3d4a122ed043081679c2ff5bea216f6eb8a71dea
Gedas1357 <gedasvilnietis@gmail.com>
2023-09-28 12:01:17 +0300

    Added css to NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,9 +5,9 @@ const NavBar = () => {
     return (
         <nav className="w-screen h-16 bg-gray-300 complete-center">
             <ul className="flex flex-row w-screen h-16 items-center justify-evenly ">
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/recipes">Recipes</Link></li>
-                <li><Link to="/ingredients">Ingredients</Link></li>
+                <li className="NavBar-li"><Link to="/">Home</Link></li>
+                <li className="NavBar-li"><Link to="/recipes">Recipes</Link></li>
+                <li className="NavBar-li"><Link to="/ingredients">Ingredients</Link></li>
             </ul>
         </nav>
     );
