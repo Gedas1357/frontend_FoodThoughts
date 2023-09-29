989f44ab57714638d086b319537075ed4f8b644e
Gedas1357 <gedasvilnietis@gmail.com>
2023-09-29 13:19:29 +0300

    Got axios ingredients response and listed them out on the page

--- src/pages/Ingredients.js
+++ src/pages/Ingredients.js
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getIngredients } from "../requests/Ingredients";
 
 const Ingredients = () => {
+    const [ingredientList, setIngredientList] = useState([]);
+
+    useEffect(() => {
+        getIngredients(setIngredientList);
+    },[]);
+
     return (
-        <div>Ingredients</div>
+        <div>
+            {ingredientList.map((ingredient, index) => {
+                return (
+                    <div key={index}>
+                        <h2>{ingredient.name}</h2>
+                    </div>
+                );
+            })}
+
+        </div>
+
     );
 }
 
