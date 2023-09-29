faa54d12f377e3d47a7871d43f9f81a9435f7726
Gedas1357 <gedasvilnietis@gmail.com>
2023-09-29 16:40:20 +0300

    Deleted comments

--- src/requests/Ingredients.js
+++ src/requests/Ingredients.js
@@ -3,14 +3,6 @@ import axios from "axios";
 export const getIngredients = (setIngredients) => {
     const response = axios.get('http://127.0.0.1:8000/ingredients')
         .then(response => {
-            //setIngredients();
             setIngredients(response.data);
-            //console.log(response.data.type);
-            //console.log(response);
-            //console.log(response.data[0]);
-            //console.log(Object.values(response.data))
-            //setIngredients(response.data[0].type);
-            //setIngredients(Object.values(response.data).name);
         })
-    //console.log(response.data);
 }
